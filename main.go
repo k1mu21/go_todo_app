93c5b035d67a30082e77a45714af5bde020cc23e
AEON-MALL <jbytq@yahoo.co.jp>
2024-05-12 00:35:03 +0900

    Jsonで色々する

--- main.go
+++ main.go
@@ -1,24 +1,17 @@
 package main
 
 import (
-	"context"
 	"log"
-	"net" // Add this import statement
-	"os"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p) // Fix the method name to "Listen"
-	if err != nil {
-		log.Fatalf("failed to listen port %s: %v", p, err) // Fix the typo "faild" to "failed"
-	}
-	if err := run(context.Background(), l); err != nil {
-		log.Printf("failed to terminate: %v", err) // Fix the typo "faild" to "failed"
-		os.Exit(1)
-	}
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello, World!"})
+	})
+
+	log.Fatal(app.Listen(":4000"))
 }
